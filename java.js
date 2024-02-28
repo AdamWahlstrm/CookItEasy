ef07702d466dcd8b04a8eb4e41e65fa49a0a6656
AdamWahlstrm <wahlstrmadam@gmail.com>
2024-02-28 15:01:01 +0100

    Initial commit

---
+++ java.js
@@ -0,0 +1,20 @@
+// Laddar in headern och navigationsmenyn från header.html på varje sida
+
+// Skapar en ny XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// Ange metod, filnamn och asynkron (true) eller synkron (false) förfrågan
+xhr.open('GET', 'header.html', true);
+
+// Funktionen som körs när förfrågan är klar
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    // Inkluderar headern och navigationsmenyn i div-elementet med id "header"
+    document.getElementById('header').innerHTML = xhr.responseText;
+  }
+};
+
+// Skickar förfrågan
+xhr.send();
+
+
